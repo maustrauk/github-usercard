254bda456994a6a38480551d5699b743cd568bac
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-29 14:30:28 -0400

    Part_1 Step_1

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
+    https://api.github.com/users/maustrauk
 */
-
+const myData = axios.get("https://api.github.com/users/maustrauk");
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
